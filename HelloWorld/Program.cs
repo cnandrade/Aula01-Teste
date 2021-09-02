b9f64862eda10b2a38bcdb7ddad195c9fb74c119
cnandrade <89871289+cnandrade@users.noreply.github.com>
2021-09-02 14:18:17 -0300

    Aula 01

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] argumentos)
+
+        {
+            int idade = 28;
+            bool isadulto = true;
+            decimal valor = 10.55m;
+            double valor2 = 10.55;
+            char sigla = 'C';
+            string estado = "Sao Paulo";
+
+            Console.WriteLine("Cleber");
+        }
+    }
+}
